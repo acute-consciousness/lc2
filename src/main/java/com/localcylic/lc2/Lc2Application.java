abcb915afdff090322a60f5fe96f68ab5b7481ca
acute-consciousness <erickmathai@gmail.com>
2026-08-13 10:11:12 +0300

    Initial commit

---
+++ src/main/java/com/localcylic/lc2/Lc2Application.java
@@ -0,0 +1,13 @@
+package com.localcylic.lc2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Lc2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Lc2Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/localcylic/lc2/Lc2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.localcylic.lc2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Lc2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
