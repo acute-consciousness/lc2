81f1fbef37e58b6a73d9729db8e03cab6c20229f
acute-consciousness <erickmathai@gmail.com>
2026-08-24 16:18:43 +0300

    Update User.java
    the serialisation ... works. I have gotten a JSON  on, on, on an endpoint

--- src/main/java/com/localcylic/lc2/entities/User.java
+++ src/main/java/com/localcylic/lc2/entities/User.java
@@ -23,6 +23,45 @@ public class User {
 		this.lattitude = lattitude;
 		this.longitude = longitude;
 	}
+
+	/*so, so this is me much later - you know, you know, you know what i mean
+	 * anyway the issue is, bpp, claude to be precise says that, says that ... a classic json problem being, being, being that
+	 *  da, da, da, the a commandlinerunner test confirms the objects are there, but the same rest mapping.. methods don't show, in the endpoint
+	 *  ...
+	 *  the solution that has led me here is something to do with getters, that i n my accounts class, in here
+	 *  that, that, that the fields, that, that the fields should be public or somthing. then i was like
+	 *  but the toString method is public. Then now i remember that toString, mmh, doesn't work
+	 *  in the whole compass of deserialization, Doesn't, mmmh, doesn't work in the whole deserialization 'encompasses/ing
+	 *  that i need the fields to be somehow, to, to, to, to be somehow visible for the de/serialization, seriaalization stuff.
+	 *  
+	 *  and the solution is for that, i have seen is the individual, now i can call, now, mmh, now i can call them gettes
+	 *  that i had, that i had,, that i had earlier done. By the way, why did i do them, why, mmh, why did i have them
+	 *  for individual/ specific field, field, field, field, returns. Mimi, mimi, mimi, mimi, mimi nilikuwa nimezifanya aje?
+	 *  just return username..., not casting, i asume ingeleta tu the @... but serialization, i think, i think, i think, i think, i think, 
+	 *  i think can, can, can handle that. So checking this out, i think i, i, i, i will just, i, i will just coment these function out
+	 *  it is still empty, or is it to do ith th naming
+	 *  the, getters, getters must start with, with, with, with the 'prefix' get or fetch that i didn't, or fetc that i haven't tried out, alafu, alafu is for  booleans
+	 *  alafu, alafu, alafu if, alafu if for booleans, sawa to the what, to the  textinput stuff
+	 *  */
+	//logic down here is that i can cast individual records(alone). the above long comment explain the usage of them now
+	//
+	public String getUsername() {
+		return userName;
+	}
+	public String getPhonenumber() {
+		return phoneNumber;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public String getLatitude() {
+		return lattitude;
+	}
+	public String getLongititude() {
+		return longitude;
+	}
+	
+	// changed the position of the  method down below, because bpp had it that wa. i haven't yet thought hwow it would or not affect things
 	public String toString() {
 		return "\n"+
 				"user name: "+userName + '\n' +
@@ -30,24 +69,6 @@ public class User {
 				 "email: "+email+ '\n' +
 				 "location: "+ lattitude + " , " +longitude;
 	}
-	/* logic down here is that i can cast individual records(alone)
-	public String returnUsername() {
-		return userName;
-	}
-	public String returnPhonenumber() {
-		return phoneNumber;
-	}
-	public String returnEmail() {
-		return email;
-	}
-	public String returnLatitude() {
-		return lattitude;
-	}
-	public String returnLongititude() {
-		return longitude;
-	}
-*/
-	
 	
 	//and this is just a model, but in saying just it's above just a 'bare' interface
 	//but how do the later defined methods work if there isn't a data structure here. Na tumesema just a model.
