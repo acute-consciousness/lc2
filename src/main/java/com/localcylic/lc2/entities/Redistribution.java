43c479676b6d07e616d793dfd95f47254fcbf560
acute-consciousness <erickmathai@gmail.com>
2026-09-11 13:51:07 +0300

    Serious start of the Redistribution field in the entities

--- src/main/java/com/localcylic/lc2/entities/Redistribution.java
+++ src/main/java/com/localcylic/lc2/entities/Redistribution.java
@@ -1,5 +1,15 @@
 package com.localcylic.lc2.entities;
 
-public class Redistribution {
+//what else, mmh, what, mmh, what, mmh, what, mmh, what, mmh, what else, what, mmh, what else was the
+//i know i have to ...a foreign key but that is in the fields.. when creating a new field
+//anyway, anyway, let us create fields for the listings, iyo jina, mmh, iyo jina listing eeew
 
+public class Redistribution {
+//fields
+	//having multiple items, mmh, we will think of that tomorrow, i think starting this from the front end would be nice
+	private String listingDescription;//again the issue of different postings that can be sold individually
+	//classification of furnitures
+//	type of furniture - yea, ii ni, ii ni ya frontend
+	//you, know, start with a json, mmh, start, mmh, start, mmh, start with a json object, i have just deen the word seed, and, and, mmh, and that is how we used to call it? i'm nt dure/ i don't know
+	
 }
